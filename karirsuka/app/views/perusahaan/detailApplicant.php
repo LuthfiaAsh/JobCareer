d00f9f984cd49514215f8c5661030a4cacc2ff2f
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2021-07-13 21:15:25 +0700

    detailApplicant.php

--- karirsuka/app/views/perusahaan/detailApplicant.php
+++ karirsuka/app/views/perusahaan/detailApplicant.php
@@ -4,33 +4,14 @@ $lowongan = $data["lowongan"];
 $applicants = $data["applicants"];
 ?>
 
-<div class="container" style="align:center; margin-top: 100px;">
-<h6 style="color:#A4A4A4">Detail Applicant</h6>
-<h4><b><?= $lowongan["judul"]; ?> - <?= $user["namaPerush"]; ?></b></h4>
+<div class="container" style="align:center; margin-top: 50px;">
+  
+<h6 style="color:#A4A4A4 text-align:left; margin-left:100px;">Detail Applicant</h6>
+<h4 style="text-align:left; margin-left:100px;"><b><?= $lowongan["judul"]; ?> - <?= $user["namaPerush"]; ?></b></h4>
 <br><br>
 
-<!-- Check box -->
 <div class="container">
-  <div class="row">
-    <div class="col-sm-4 border shadow-sm p-3 mb-5 bg-body rounded">
-        <h6>Pengalaman Kerja</h6>
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" style="color:#A4A4A4" for="flexCheckDefault">Fresh Graduate</label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-            <label class="form-check-label" style="color:#A4A4A4" for="flexCheckChecked">1-2 tahun</label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" style="color:#A4A4A4" for="flexCheckDefault">3-5 tahun</label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-            <label class="form-check-label" style="color:#A4A4A4" for="flexCheckChecked">>5 tahun</label>
-        </div>
-    </div>
+  <div class="row row-centered">
 
     <div class="col-sm-8 border shadow-sm p-3 mb-5 bg-body rounded">
         <div class="container">
@@ -40,21 +21,21 @@ $applicants = $data["applicants"];
 
             <div class="card p-4">
               <div class="row">
-                <div class="col-auto">
+                  <div class="col d-flex align-items-center mr-3">
                   <?= '<img src="data:image/jpeg;base64,' . base64_encode($applicant["photo"]) . '" alt="Logo" style="width:120px;">' ?>
                 </div>
-                <br><br>
-                <div class="col-auto me-3">
-                  <h4 style="text-align:left;"><b><?= $applicant["nama"]; ?></b></h4>
+ 
+                <div class="col-6">
+                  <h4 style="text-align:left;  margin-top:30px;"><b><?= $applicant["nama"]; ?></b></h4>
                   <h8 style="color:#676767;">Skill : <?= $applicant["kompetensi"];; ?></h8><br>
                   <h8 style="color:#676767;">Prodi : <?= $applicant["programStudi"];; ?></h8><br>
                   <h8 style="color:#676767;">IPK : <?= $applicant["ipk"];; ?></h8><br>
                 </div>
                
-                <div class="col-auto mt-4 mb-2 ps-5">
-                    <h8 style="text-align:right; color:#8A8A8A;">Contact:</h8><br>
-                    <h8 style="color:#676767;"><?= $applicant["hp_skrg"];; ?></h8><br>
-                    <h8 style="color:#676767;"><?= $applicant["email"];; ?></h8>
+                <div class="col my-5">
+                    <h6 style="text-align:right; color:#8A8A8A; font-size:12px; margin-right:150px">Contact:</h6><br>
+                    <h8 style="text-align:right; font-size:12px; color:#1F5938; margin-right:25px"><?= $applicant["hp_skrg"];; ?></h8><br>
+                    <h8 style="text-align:right; font-size:12px; color:#1F5938; margin-right:25px"><?= $applicant["email"];; ?></h8>
                 </div>
                 <a href="<?= BASE_URL ?>perusahaan/history/<?= $applicant["idLowongan"]; ?>" class="stretched-link"></a>
 
